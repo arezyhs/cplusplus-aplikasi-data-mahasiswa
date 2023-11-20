42a08af6ff4bc8ac9641f00b5db35ff06000a008
Akbar Setiyawan <akbar.23335@mhs.unesa.ac.id>
2023-11-20 21:55:33 +0700

    Update aplikasi-data-mahasiswa.cpp
    allows user to input more than one data and stores the inputted data into txt.file.

--- aplikasi-data-mahasiswa.cpp
+++ aplikasi-data-mahasiswa.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <fstream>
 using namespace std;
 
 /*
     Created by Akbar Setiyawan
     arezyh.s
-    */
+*/
 
-// function here!
-// validate only digits for nim
-bool is_nim_valid(const string &nim) {
-    for (char c : nim) {
-        if (!isdigit(c)) {
-            return false;
-        }
-    }
-    return true;
-}
+struct mahasiswa {
+    string namaLengkap;
+    string nim;
+    string fakultas;
+    string programStudi;
+    string kotaAsal;
+    string noTelp;
+};
 
-// validae only digits for phone number
-bool is_phone_valid(const string &no_telp) {
-    for (char c : no_telp) {
-        if (!isdigit(c)) {
+bool isNumber(const string& str) {
+    for (char ch : str) {
+        if (!isdigit(ch)) {
             return false;
         }
     }
@@ -31,67 +29,73 @@ bool is_phone_valid(const string &no_telp) {
 
 // main code here!
 int main() {
-    // variabel here!
-    string nama_lengkap, nim, fakultas, program_studi, kota_asal, no_telp;
+    const int jumlahDataMahasiswa = 100;
 
     // app title
     cout << "#       DATA MAHASISWA       #" << endl;
     cout << "==============================" << endl;
 
-    while (true) {
-        // request input from user.
-        cout << "Masukkan data berikut: " << endl;
-        cout << "1. Nama Lengkap   : ";
-        getline(cin, nama_lengkap);
+    mahasiswa dataMahasiswa[jumlahDataMahasiswa];
+    char lanjut;
+    int i = 0;
 
-        // input user nim
-        while (true) {
-            cout << "2. NIM            : ";
-            getline(cin, nim);
-            if (is_nim_valid(nim)) {
-                break;
-            } else {
-                cout << "NIM yang dimasukkan tidak valid. Pastikan NIM hanya berisi angka." << endl;
+    do {
+        system("cls");
+        cout << "Input data mahasiswa ke-" << i + 1 << ":" << endl;
+
+        cout << "1. Nama lengkap    : ";
+        getline(cin, dataMahasiswa[i].namaLengkap);
+        
+        // NIM validation
+        do {
+            cout << "2. NIM             : ";
+            getline(cin, dataMahasiswa[i].nim);
+            if (!isNumber(dataMahasiswa[i].nim)) {
+                cout << "NIM harus berupa angka. Silakan masukkan kembali." << endl;
             }
-        }
+        } while (!isNumber(dataMahasiswa[i].nim));
 
-        cout << "3. Fakultas       : ";
-        getline(cin, fakultas);
-        cout << "4. Program Studi  : ";
-        getline(cin, program_studi);
-        cout << "5. Kota Asal      : ";
-        getline(cin, kota_asal);
-
-        while (true) {
-            cout << "6. No Telp        : ";
-            getline(cin, no_telp);
-            if (is_phone_valid(no_telp)) {
-                break;
-            } else {
-                cout << "Nomor Telepon yang dimasukkan tidak valid. Pastikan nomor telepon hanya berisi angka." << endl;
+        cout << "3. Fakultas        : ";
+        getline(cin, dataMahasiswa[i].fakultas);
+        cout << "4. Program Studi   : ";
+        getline(cin, dataMahasiswa[i].programStudi);
+        cout << "5. Kota Asal       : ";
+        getline(cin, dataMahasiswa[i].kotaAsal);
+        
+        // Nomor Telepon (noTelp) validation
+        do {
+            cout << "6. Nomor Telepon   : ";
+            getline(cin, dataMahasiswa[i].noTelp);
+            if (!isNumber(dataMahasiswa[i].noTelp)) {
+                cout << "Nomor Telepon harus berupa angka. Silakan masukkan kembali." << endl;
             }
+        } while (!isNumber(dataMahasiswa[i].noTelp));
+
+        cout << "Lanjut memasukkan data mahasiswa (Y/N): ";
+        cin >> lanjut;
+        cin.ignore();
+
+        i++;
+    } while ((lanjut == 'Y' || lanjut == 'y') && i < jumlahDataMahasiswa);
+
+    ofstream file("DATA_MAHASISWA.txt");
+    if (file.is_open()) {
+        file << "***** DATA MAHASISWA *****" << endl;
+        for (int j = 0; j < i; ++j) {
+            file << "Data Mahasiswa ke-" << j + 1 << ":" << endl;
+            file << "1. Nama Lengkap   : " << dataMahasiswa[j].namaLengkap << endl;
+            file << "2. NIM            : " << dataMahasiswa[j].nim << endl;
+            file << "3. Fakultas       : " << dataMahasiswa[j].fakultas << endl;
+            file << "4. Program Studi  : " << dataMahasiswa[j].programStudi << endl;
+            file << "5. Kota Asal      : " << dataMahasiswa[j].kotaAsal << endl;
+            file << "6. Nomor Telepon  : " << dataMahasiswa[j].noTelp << endl;
+            file << endl;
         }
-
-        // show their inputted data
-        cout << " " << endl;
-        cout << "Data berhasil dimasukkan! " << endl;
-        cout << "Berikut data yang telah anda masukkan: " << endl;
-        cout << "Data Mahasiswa: " << endl;
-        cout << "1. Nama           : " << nama_lengkap << endl;
-        cout << "2. NIM            : " << nim << endl;
-        cout << "3. Fakultas       : " << fakultas << endl;
-        cout << "4. Program Studi  : " << program_studi << endl;
-        cout << "5. Kota Asal      : " << kota_asal << endl;
-        cout << "6. Nomor Telepon  : " << no_telp << endl;
-
-        cout << "Apakah Anda ingin mengupdate data (Y/N)? " << endl;
-        string konfirmasi;
-        cin >> konfirmasi;
-
-        if (konfirmasi == "N" || konfirmasi == "n") {
-            break;
-        }
+        file.close();
+        system("cls");
+        cout << "Berhasil disimpan ke dalam file DATA_MAHASISWA.txt" << endl;
+    } else {
+        cout << "Gagal membuka file untuk penulisan." << endl;
     }
-
     return 0;
 }
